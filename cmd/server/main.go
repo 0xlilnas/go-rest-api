8b40274b8f27cd9d6d27502cf216033dd419e56d
Naz M <nazlee41@gmail.com>
2022-08-02 16:16:05 +0800

    chore: structuring application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,23 @@
 package server
+
+import "fmt"
+
+// struct which contains pointer
+// database connections
+type App struct {
+}
+
+//setting up application
+func (app *App) Run() error {
+	fmt.Println("Setting up our app")
+	return nil
+}
+
+func main() {
+	fmt.Println("Go rest api")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our rest api")
+		fmt.Println(err)
+	}
+}
